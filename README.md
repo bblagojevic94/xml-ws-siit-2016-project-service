be6096c5bd754fe977c1103327df77aa0a17fefa
Bojan Blagojević <datiglavaradi@gmail.com>
2016-11-12 17:19:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# xml-ws-siit-2016-project-service
+Predmetni projekat iz predmeta XML i web servisi.
+
+# Članovi tima:
+- SW3-2013  Stefan Ristanović
+- SW9-2013  Bojan Blagojević
+- SWF-2013  Dragutin Marjanović
---
+++ src/main/java/rs/ac/uns/sw/xml/XmlWsProject2016Application.java
@@ -0,0 +1,12 @@
+package rs.ac.uns.sw.xml;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlWsProject2016Application {
+    public static void main(String[] args) {
+        SpringApplication.run(XmlWsProject2016Application.class, args);
+    }
+}
